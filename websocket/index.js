871b63a4d8110586e8b235ab27cc755f9701bb9b
akshayp0022 <akshay.pawar@bhartisofttech.com>
2024-11-13 15:22:12 +0530

    Update index.js socket

--- websocket/index.js
+++ websocket/index.js
@@ -27,7 +27,7 @@ const logger = winston.createLogger({
 function socketServer(server) {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "*",
       methods: ["GET", "POST"],
       credentials: true,
     },
